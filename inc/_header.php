03bba82896f012de559393102921fc85e2c15bb0
Jonata Weber <jonataa@gmail.com>
2020-03-31 22:05:07 -0300

    share the website with team

---
+++ inc/_header.php
@@ -0,0 +1,72 @@
+<!-- ================================
+            START HEADER AREA
+================================= -->
+<header class="header-area header-area2">
+    <div class="header-top header-menu-action">
+        <div class="container">
+            <div class="row ostion-top-wrap">
+                <div class="col-lg-7 col-sm-7 site-branding">
+                    <div class="logo-action d-flex align-items-center">
+                        <div class="ostion-logo">
+                            <a href="https://www.coronavidas.net">
+                                <img src="images/logo2.png" width="200" alt="Corona Vidas" title="Corona Vidas">
+                            </a>
+                        </div><!-- .ostion-logo -->
+                        <div class="header-btn ml-auto">
+                        <a target="_blank" href="http://vaka.me/956493" class="theme-btn">DOAR AGORA</a>
+                                    <a target="_blank" href="https://chat.whatsapp.com/DcL189qM2WIHr8K0X1THmf" class="theme-btn" style="margin-left: 30px">SEJA VOLUNTÁRIO</a>
+                        </div>
+                    </div><!-- end logo-action -->
+                </div><!-- site-branding -->
+                <div class="col-lg-5 col-sm-5 ostion-menu">
+                    <div class="ostion-menu-innner">
+                        <div class="ostion-menu-content">
+                            <div class="navigation-top">
+                            <nav class="main-navigation">
+                                    <ul>
+                                        <li class="active"><a href="#">Home</a></li>
+                                        <!-- <li><a href="#">Cidades (HUBs)</a>
+                                            <ul class="dropdown-menu-item">
+                                                <li><a href="feira-de-santana.php">Feira de Santana, BA</a></li>
+                                                <li><a href="vitoria-da-conquista.php">Vitória da Conquista, BA</a></li>
+                                                <li><a href="jequie.php">Jequié, BA</a></li>
+                                                <li><a href="rio-de-janeiro.php">Rio de Janeiro, RJ</a></li>
+                                                <li><a href="sao-paulo.php">São Paulo, SP</a></li>
+                                                <li><a href="ilheus.php">Ilhéus, BA</a></li>
+                                                <li><a href="valenca.php">Valença, BA</a></li>
+                                                <li><a href="guanambi.php">Guanambi, BA</a></li>
+                                                <li><a href="itabuna.php">Itabuna, BA</a></li>
+                                            </ul>
+                                        </li> -->
+                                        <li><a href="contato.php">Contato</a></li>
+                                    </ul>
+                                </nav><!-- end main-navigation -->
+                            </div><!-- end navigation-top -->
+                        </div><!-- end ostion-menu-content -->
+                        <div class="mobile-menu-toggle">
+                            <i class="fa fa-bars"></i>
+                        </div>
+                    </div><!-- end ostion-menu-innner -->
+                </div><!-- ostion-menu -->
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </div><!-- end header-top -->
+    <div class="side-nav-container">
+        <div class="humburger-menu">
+            <div class="humburger-menu-lines side-menu-close"></div><!-- end humburger-menu-lines -->
+        </div><!-- end humburger-menu -->
+        <div class="side-menu-wrap">
+            <ul class="side-menu-ul">
+              <li class="sidenav__item"><a href="index.php">Home</a></li>
+              <li class="sidenav__item"><a href="contato.php">Contato</a></li>
+            </ul>
+            <div class="side-btn">
+                <a href="http://vaka.me/956493" class="theme-btn">DOAR AGORA</a>
+                <a target="_blank" href="https://chat.whatsapp.com/DcL189qM2WIHr8K0X1THmf" class="theme-btn" style="margin-top: 30px">SEJA VOLUNTÁRIO</a>
+            </div>
+        </div><!-- end side-menu-wrap -->
+    </div><!-- end side-nav-container -->
+</header>
+<!-- ================================
+         END HEADER AREA
+================================= -->
