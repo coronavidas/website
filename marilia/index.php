c6bccbeefad43aa4a1169c8845494c9b251c9ebe
Ruanderson Vieira <ruanderson.vieira@accalia.io>
2020-04-14 11:51:55 -0300

    :bug: marilia routes

--- marilia/index.php
+++ marilia/index.php
@@ -5,22 +5,22 @@
         <title>Faça a sua doação para o Corona Vidas de São Paulo</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="icon" href="marilia/images/favicon.png">
+        <link rel="icon" href="./images/favicon.png">
 
           <link href="https://fonts.googleapis.com/css?family=Mansalva|Roboto&display=swap" rel="stylesheet">
 
-          <link rel="stylesheet" href="marilia/fonts/icomoon/style.css">
+          <link rel="stylesheet" href="./fonts/icomoon/style.css">
 
-          <link rel="stylesheet" href="marilia/css/bootstrap.min.css">
-          <link rel="stylesheet" href="marilia/css/animate.min.css">
-          <link rel="stylesheet" href="marilia/css/jquery.fancybox.min.css">
-          <link rel="stylesheet" href="marilia/css/owl.carousel.min.css">
-          <link rel="stylesheet" href="marilia/css/owl.theme.default.min.css">
-          <link rel="stylesheet" href="marilia/fonts/flaticon/font/flaticon.css">
-          <link rel="stylesheet" href="marilia/css/aos.css">
+          <link rel="stylesheet" href="./css/bootstrap.min.css">
+          <link rel="stylesheet" href="./css/animate.min.css">
+          <link rel="stylesheet" href="./css/jquery.fancybox.min.css">
+          <link rel="stylesheet" href="./css/owl.carousel.min.css">
+          <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+          <link rel="stylesheet" href="./fonts/flaticon/font/flaticon.css">
+          <link rel="stylesheet" href="./css/aos.css">
 
           <!-- MAIN CSS -->
-          <link rel="stylesheet" href="marilia/css/style.css">
+          <link rel="stylesheet" href="./css/style.css">
 
         </head>
 
@@ -30,9 +30,9 @@
               <div class="container">
                 <div class="row align-items-center position-relative">
                   <div class="site-logo">
-                    <a href="http://coronavidas.net/" class="text-white"><img src="marilia/images/logo.png"</a>
+                    <a href="http://coronavidas.net/" class="text-white"><img src="./images/logo.png"</a>
                   </div>
-                  <div class="ml-auto toggle-button d-inline-block d-lg-none"><a href="marilia/#" class="site-menu-toggle py-5 js-menu-toggle text-white"><span class="icon-menu h3 text-white"></span></a></div>
+                  <div class="ml-auto toggle-button d-inline-block d-lg-none"><a href="./#" class="site-menu-toggle py-5 js-menu-toggle text-white"><span class="icon-menu h3 text-white"></span></a></div>
                 </div>
               </div>
             </header>
@@ -49,25 +49,25 @@
               <div class="row">
                 <div class="col-md-3">
                     <div class="cause shadow-sm">
-                      <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=457514312618D03A" class="cause-link d-block"><img src="marilia/images/10reais.jpg" alt="Image" class="img-fluid"></a>
+                      <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=457514312618D03A" class="cause-link d-block"><img src="./images/10reais.jpg" alt="Image" class="img-fluid"></a>
                     </div>
                 </div>
 
                 <div class="col-md-3">
                   <div class="cause shadow-sm">
-                    <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=E23C61062D9FBE0C" class="cause-link d-block"><img src="marilia/images/50reais.jpg" alt="Image" class="img-fluid"></a>
+                    <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=E23C61062D9FBE0C" class="cause-link d-block"><img src="./images/50reais.jpg" alt="Image" class="img-fluid"></a>
                   </div>
                 </div>
 
                 <div class="col-md-3">
                   <div class="cause shadow-sm">
-                    <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=5A3F3431272E8A2B" class="cause-link d-block"><img src="marilia/images/100reais.jpg" alt="Image" class="img-fluid"></a>
+                    <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=5A3F3431272E8A2B" class="cause-link d-block"><img src="./images/100reais.jpg" alt="Image" class="img-fluid"></a>
                   </div>
                 </div>
         
                 <div class="col-md-3">
                   <div class="cause shadow-sm">
-                    <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=B4DDE8CB7C858D12" class="cause-link d-block"><img src="marilia/images/1000reais.jpg" alt="Image" class="img-fluid"></a>
+                    <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=B4DDE8CB7C858D12" class="cause-link d-block"><img src="./images/1000reais.jpg" alt="Image" class="img-fluid"></a>
                   </div>
                 </div>
                 
@@ -75,16 +75,16 @@
             </div>
           </div>
 
-          <script src="marilia/js/jquery-3.3.1.min.js"></script>
-          <script src="marilia/js/popper.min.js"></script>
-          <script src="marilia/js/bootstrap.min.js"></script>
-          <script src="marilia/js/owl.carousel.min.js"></script>
-          <script src="marilia/js/jquery.sticky.js"></script>
-          <script src="marilia/js/jquery.waypoints.min.js"></script>
-          <script src="marilia/js/jquery.animateNumber.min.js"></script>
-          <script src="marilia/js/jquery.fancybox.min.js"></script>
-          <script src="marilia/js/jquery.easing.1.3.js"></script>
-          <script src="marilia/js/aos.js"></script>
-          <script src="marilia/js/main.js"></script>
+          <script src="./js/jquery-3.3.1.min.js"></script>
+          <script src="./js/popper.min.js"></script>
+          <script src="./js/bootstrap.min.js"></script>
+          <script src="./js/owl.carousel.min.js"></script>
+          <script src="./js/jquery.sticky.js"></script>
+          <script src="./js/jquery.waypoints.min.js"></script>
+          <script src="./js/jquery.animateNumber.min.js"></script>
+          <script src="./js/jquery.fancybox.min.js"></script>
+          <script src="./js/jquery.easing.1.3.js"></script>
+          <script src="./js/aos.js"></script>
+          <script src="./js/main.js"></script>
         </body>
       </html>
