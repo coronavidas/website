5e076105c141d2e99d83fb3fe6eeaa694be92737
Ruanderson Vieira <ruanderson.vieira@accalia.io>
2020-04-14 12:17:49 -0300

    :bug: marilia and sp routes

--- marilia/index.php
+++ marilia/index.php
@@ -1,90 +1,126 @@
-      <!DOCTYPE html>
-      <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-      <head>
-        <title>Faça a sua doação para o Corona Vidas de São Paulo</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="icon" href="./images/favicon.png">
+<head>
+  <title>Faça a sua doação para o Corona Vidas de Marília</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" href="./images/favicon.png">
 
-          <link href="https://fonts.googleapis.com/css?family=Mansalva|Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Mansalva|Roboto&display=swap" rel="stylesheet">
 
-          <link rel="stylesheet" href="./fonts/icomoon/style.css">
+    <link rel="stylesheet" href="./fonts/icomoon/style.css">
 
-          <link rel="stylesheet" href="./css/bootstrap.min.css">
-          <link rel="stylesheet" href="./css/animate.min.css">
-          <link rel="stylesheet" href="./css/jquery.fancybox.min.css">
-          <link rel="stylesheet" href="./css/owl.carousel.min.css">
-          <link rel="stylesheet" href="./css/owl.theme.default.min.css">
-          <link rel="stylesheet" href="./fonts/flaticon/font/flaticon.css">
-          <link rel="stylesheet" href="./css/aos.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/animate.min.css">
+    <link rel="stylesheet" href="./css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="./css/owl.carousel.min.css">
+    <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="./fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="./css/aos.css">
 
-          <!-- MAIN CSS -->
-          <link rel="stylesheet" href="./css/style.css">
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="./css/style.css">
 
-        </head>
+  </head>
 
-        <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
-          <div class="site-wrap" id="home-section">
-              <header class="site-navbar site-navbar-target bg-secondary shadow" role="banner">
-              <div class="container">
-                <div class="row align-items-center position-relative">
-                  <div class="site-logo">
-                    <a href="http://coronavidas.net/" class="text-white"><img src="./images/logo.png"</a>
-                  </div>
-                  <div class="ml-auto toggle-button d-inline-block d-lg-none"><a href="./#" class="site-menu-toggle py-5 js-menu-toggle text-white"><span class="icon-menu h3 text-white"></span></a></div>
-                </div>
-              </div>
-            </header>
-          <div class="site-section">
-            <div class="container">
-              <div class="row mb-5 align-items-st">
-                <div class="col-md-4">
-                  <div class="heading-20219">
-                    <h2 class="title text-cursive">Faça a sua Doação para: Corona Vidas - São Paulo</h2>
-                  </div>
-                </div>
-              </div>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
 
-              <div class="row">
-                <div class="col-md-3">
-                    <div class="cause shadow-sm">
-                      <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=457514312618D03A" class="cause-link d-block"><img src="./images/10reais.jpg" alt="Image" class="img-fluid"></a>
-                    </div>
-                </div>
+    
+    <div class="site-wrap" id="home-section">
 
-                <div class="col-md-3">
-                  <div class="cause shadow-sm">
-                    <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=E23C61062D9FBE0C" class="cause-link d-block"><img src="./images/50reais.jpg" alt="Image" class="img-fluid"></a>
-                  </div>
-                </div>
+      
+         <header class="site-navbar site-navbar-target bg-secondary shadow" role="banner">
 
-                <div class="col-md-3">
-                  <div class="cause shadow-sm">
-                    <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=5A3F3431272E8A2B" class="cause-link d-block"><img src="./images/100reais.jpg" alt="Image" class="img-fluid"></a>
-                  </div>
-                </div>
+        <div class="container">
+          <div class="row align-items-center position-relative">
+            <div class="site-logo">
+              <a href="http://coronavidas.net/" class="text-white"><img src="./images/logo.png"</a>
+            </div>
+
+
+            
+
+
+            <div class="ml-auto toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle text-white"><span class="icon-menu h3 text-white"></span></a></div>
+
+            
+
+          </div>
+        </div>
+
+      </header>
+
+
+
+
+   
+
+    <div class="site-section">
+      <div class="container">
+         
+          
         
-                <div class="col-md-3">
-                  <div class="cause shadow-sm">
-                    <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=B4DDE8CB7C858D12" class="cause-link d-block"><img src="./images/1000reais.jpg" alt="Image" class="img-fluid"></a>
-                  </div>
-                </div>
-                
+        <div class="row mb-5 align-items-st">
+          <div class="col-md-4">
+            <div class="heading-20219">
+              <h2 class="title text-cursive">Faça a sua Doação para: Corona Vidas - Marília</h2>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-3">
+              <div class="cause shadow-sm">
+                <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=9BDF24CFAB3A7587" class="cause-link d-block"><img src="./images/10reais.jpg" alt="Image" class="img-fluid"></a>
               </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="cause shadow-sm">
+              <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=627D53FB95CD2A5D" class="cause-link d-block"><img src="./images/50reais.jpg" alt="Image" class="img-fluid"></a>
             </div>
           </div>
 
-          <script src="./js/jquery-3.3.1.min.js"></script>
-          <script src="./js/popper.min.js"></script>
-          <script src="./js/bootstrap.min.js"></script>
-          <script src="./js/owl.carousel.min.js"></script>
-          <script src="./js/jquery.sticky.js"></script>
-          <script src="./js/jquery.waypoints.min.js"></script>
-          <script src="./js/jquery.animateNumber.min.js"></script>
-          <script src="./js/jquery.fancybox.min.js"></script>
-          <script src="./js/jquery.easing.1.3.js"></script>
-          <script src="./js/aos.js"></script>
-          <script src="./js/main.js"></script>
-        </body>
-      </html>
+          <div class="col-md-3">
+            <div class="cause shadow-sm">
+              <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=5A3F3431272E8A2B" class="cause-link d-block"><img src="./images/100reais.jpg" alt="Image" class="img-fluid"></a>
+            </div>
+          </div>
+  
+          <div class="col-md-3">
+            <div class="cause shadow-sm">
+              <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=B4DDE8CB7C858D12" class="cause-link d-block"><img src="./images/1000reais.jpg" alt="Image" class="img-fluid"></a>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+
+
+
+    </div>
+    
+   
+    
+    
+
+    </div>
+
+    <script src="./js/jquery-3.3.1.min.js"></script>
+    <script src="./js/popper.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/owl.carousel.min.js"></script>
+    <script src="./js/jquery.sticky.js"></script>
+    <script src="./js/jquery.waypoints.min.js"></script>
+    <script src="./js/jquery.animateNumber.min.js"></script>
+    <script src="./js/jquery.fancybox.min.js"></script>
+    <script src="./js/jquery.easing.1.3.js"></script>
+    <script src="./js/aos.js"></script>
+
+    <script src="./js/main.js"></script>
+
+  </body>
+
+</html>
---
+++ sp/index.php
@@ -0,0 +1,90 @@
+      <!DOCTYPE html>
+      <html lang="en">
+
+      <head>
+        <title>Faça a sua doação para o Corona Vidas de São Paulo</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" href="./images/favicon.png">
+
+          <link href="https://fonts.googleapis.com/css?family=Mansalva|Roboto&display=swap" rel="stylesheet">
+
+          <link rel="stylesheet" href="./fonts/icomoon/style.css">
+
+          <link rel="stylesheet" href="./css/bootstrap.min.css">
+          <link rel="stylesheet" href="./css/animate.min.css">
+          <link rel="stylesheet" href="./css/jquery.fancybox.min.css">
+          <link rel="stylesheet" href="./css/owl.carousel.min.css">
+          <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+          <link rel="stylesheet" href="./fonts/flaticon/font/flaticon.css">
+          <link rel="stylesheet" href="./css/aos.css">
+
+          <!-- MAIN CSS -->
+          <link rel="stylesheet" href="./css/style.css">
+
+        </head>
+
+        <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+          <div class="site-wrap" id="home-section">
+              <header class="site-navbar site-navbar-target bg-secondary shadow" role="banner">
+              <div class="container">
+                <div class="row align-items-center position-relative">
+                  <div class="site-logo">
+                    <a href="http://coronavidas.net/" class="text-white"><img src="./images/logo.png"</a>
+                  </div>
+                  <div class="ml-auto toggle-button d-inline-block d-lg-none"><a href="./#" class="site-menu-toggle py-5 js-menu-toggle text-white"><span class="icon-menu h3 text-white"></span></a></div>
+                </div>
+              </div>
+            </header>
+          <div class="site-section">
+            <div class="container">
+              <div class="row mb-5 align-items-st">
+                <div class="col-md-4">
+                  <div class="heading-20219">
+                    <h2 class="title text-cursive">Faça a sua Doação para: Corona Vidas - São Paulo</h2>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-3">
+                    <div class="cause shadow-sm">
+                      <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=457514312618D03A" class="cause-link d-block"><img src="./images/10reais.jpg" alt="Image" class="img-fluid"></a>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                  <div class="cause shadow-sm">
+                    <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=E23C61062D9FBE0C" class="cause-link d-block"><img src="./images/50reais.jpg" alt="Image" class="img-fluid"></a>
+                  </div>
+                </div>
+
+                <div class="col-md-3">
+                  <div class="cause shadow-sm">
+                    <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=5FE749A3FB0EF0B9" class="cause-link d-block"><img src="./images/100reais.jpg" alt="Image" class="img-fluid"></a>
+                  </div>
+                </div>
+        
+                <div class="col-md-3">
+                  <div class="cause shadow-sm">
+                    <a target="_blank" href="https://pay.juno.com.br/checkout.html?code=21F06874A3D18DD5" class="cause-link d-block"><img src="./images/1000reais.jpg" alt="Image" class="img-fluid"></a>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+
+          <script src="./js/jquery-3.3.1.min.js"></script>
+          <script src="./js/popper.min.js"></script>
+          <script src="./js/bootstrap.min.js"></script>
+          <script src="./js/owl.carousel.min.js"></script>
+          <script src="./js/jquery.sticky.js"></script>
+          <script src="./js/jquery.waypoints.min.js"></script>
+          <script src="./js/jquery.animateNumber.min.js"></script>
+          <script src="./js/jquery.fancybox.min.js"></script>
+          <script src="./js/jquery.easing.1.3.js"></script>
+          <script src="./js/aos.js"></script>
+          <script src="./js/main.js"></script>
+        </body>
+      </html>
